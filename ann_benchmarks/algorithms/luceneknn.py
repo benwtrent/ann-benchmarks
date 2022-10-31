7d1db2dedffac19210317d636375efaf0c84af6c
Benjamin Trent <4357155+benwtrent@users.noreply.github.com>
2022-10-31 08:59:36 -0400

    Add Lucene KNN benchmarks using PyLucene

--- ann_benchmarks/algorithms/luceneknn.py
+++ ann_benchmarks/algorithms/luceneknn.py
@@ -3,92 +3,121 @@ ann-benchmarks interface for Apache Lucene.
 """
 
 import sklearn.preprocessing
-from struct import Struct
-import subprocess
-import sys
+import numpy as np
+from multiprocessing.pool import ThreadPool
+
+
+import lucene
+from lucene import JArray
+from java.nio.file import Paths
+from org.apache.lucene.store import FSDirectory
+from org.apache.lucene.search import KnnVectorQuery, IndexSearcher
+from org.apache.lucene.index import IndexWriter, IndexWriterConfig, VectorSimilarityFunction, DirectoryReader
+from org.apache.lucene.codecs.lucene91 import Lucene91Codec, Lucene91HnswVectorsFormat
+from org.apache.lucene.document import Document, FieldType, KnnVectorField, StoredField
 
 from ann_benchmarks.algorithms.base import BaseANN
 
-class LuceneBatch(BaseANN):
+
+class Codec(Lucene91Codec):
+    def __init__(self, M, efConstruction):
+        super(Codec, self).__init__()
+        self.M = M
+        self.efConstruction = efConstruction
+
+    def getKnnVectorsFormatForField(self):
+        Lucene91HnswVectorsFormat(self.M, self.efConstruction)
+
+
+class LucenePyLucene(BaseANN):
     """
     KNN using the Lucene Vector datatype.
     """
 
     def __init__(self, metric: str, dimension: int, param):
-        self.name = f"luceneknn dim={dimension} {param}"
+        lucene.initVM(vmargs=['-Djava.awt.headless=true -Xmx6g -Xms6g'])
         self.metric = metric
         self.dimension = dimension
         self.param = param
-        self.short_name = f"luceneknn-{dimension}-{param['M']}-{param['efConstruction']}"
+        self.short_name = f"lucenepyknn-{dimension}-{param['M']}-{param['efConstruction']}"
         self.n_iters = -1
         self.train_size = -1
-        #if self.metric not in ("euclidean", "angular"):
-        if self.metric != "angular":
+        self.simFunc = VectorSimilarityFunction.DOT_PRODUCT if self.metric == "angular" \
+            else VectorSimilarityFunction.EUCLIDEAN
+        if self.metric not in ("euclidean", "angular"):
             raise NotImplementedError(f"Not implemented for metric {self.metric}")
+
+    def done(self):
+        if self.dir:
+            self.dir.close()
 
     def fit(self, X):
         # X is a numpy array
         if self.dimension != X.shape[1]:
-            raise ArgumentError(f"Configured dimension {self.dimension} but data has shape {X.shape}")
+            raise Exception(f"Configured dimension {self.dimension} but data has shape {X.shape}")
         if self.metric == 'angular':
             X = sklearn.preprocessing.normalize(X, axis=1, norm='l2')
         self.train_size = X.shape[0]
-        filename = self.short_name + ".train"
-        with open(filename, "wb") as f:
-            X.tofile(f, format='f32')
-        self.knn_tester('-ndoc', str(X.shape[0]),
-                        '-reindex',
-                        '-docs', filename,
-                        '-maxConn', str(self.param['M']),
-                        '-beamWidthIndex', str(self.param['efConstruction']))
+        iwc = IndexWriterConfig().setOpenMode(IndexWriterConfig.OpenMode.CREATE)
+        codec = Codec(self.param['M'], self.param['efConstruction'])
+        iwc.setCodec(codec)
+        iwc.setRAMBufferSizeMB(1994.0)
+        self.dir = FSDirectory.open(Paths.get(self.short_name + ".index"))
+        iw = IndexWriter(self.dir, iwc)
+        fieldType = KnnVectorField.createFieldType(self.dimension, self.simFunc)
+        id = 0
+        X = X.tolist()
+        for x in X:
+            doc = Document()
+            doc.add(KnnVectorField("knn", JArray('float')(x), fieldType))
+            doc.add(StoredField("id", id))
+            id += 1
+            iw.addDocument(doc)
+            if id + 1 % 1000 == 0:
+                print(f"written {id} docs")
+        iw.forceMerge(1)
+        print(f"written {id} docs")
+        iw.close()
+        self.searcher = IndexSearcher(DirectoryReader.open(self.dir))
 
     def set_query_arguments(self, fanout):
+        self.name = f"lucenepyknn dim={self.dimension} {self.param}"
         self.fanout = fanout
 
     def query(self, q, n):
-        raise NotImplementedError(f"Single query testing not implemented: use -batch mode only")
+        if self.metric == 'angular':
+            q = q / np.linalg.norm(q)
+        return self.run_knn_query(n + self.fanout, n, q.tolist())
 
     def prepare_batch_query(self, X, n):
         if self.metric == 'angular':
             X = sklearn.preprocessing.normalize(X, axis=1, norm='l2')
-        data_filename = self.short_name + ".test"
-        with open(data_filename, "w") as f:
-            X.tofile(f)
-        self.n_iters = X.shape[0]
+        self.queries = X.tolist()
         self.topK = n
-        
+
     def run_batch_query(self):
-        data_filename = self.short_name + ".test"
-        output_filename = self.short_name + ".out"
-        self.knn_tester('-niter', str(self.n_iters), '-topK', str(self.topK),
-                        '-fanout', str(self.fanout),
-                        '-maxConn', str(self.param['M']),
-                        '-beamWidthIndex', str(self.param['efConstruction']),
-                        '-search', data_filename,
-                        '-warm', '0',
-                        '-docs', self.short_name + '.train',
-                        '-out', output_filename)
+        self.res = []
+        append = self.res.append
+        n = self.topK
+        num_candidates = self.topK + self.fanout
+        for q in self.queries:
+            append(self.run_knn_query(num_candidates=num_candidates, n=n, q=q))
 
     def get_batch_results(self):
-        output_filename = self.short_name + ".out"
-        batch_res = []
-        with open(output_filename, 'rb') as results:
-            fmt = Struct('i' * self.topK)
-            for i in range(self.n_iters):
-                res = fmt.unpack(results.read(self.topK * 4))
-                if max(res) >= self.train_size:
-                    raise AssertionError("{res} >= {self.train_size} at {i}, results={res[0]},{res[1]},{res[2]}...")
-                batch_res.append(res)
-        assert len(batch_res) == self.n_iters
-        return batch_res
+        return self.res
 
-    def knn_tester(self, *args):
-        cmd = ['java',
-               '-cp', 'lib/*:classes',
-               '-Xmx2g', '-Xms2g',
-               'org.apache.lucene.util.hnsw.KnnGraphTester',
-               '-dim', str(self.dimension)
-        ] + list(args)
-        sys.stderr.write(str(cmd))
-        subprocess.run(cmd)
+    def run_knn_query(self, num_candidates, n, q):
+        query = KnnVectorQuery("knn", JArray('float')(q), num_candidates)
+        topdocs = self.searcher.search(query, n)
+        return [d.doc for d in topdocs.scoreDocs]
 
+    def batch_query(self, X, n):
+        """Provide all queries at once and let algorithm figure out
+           how to handle it. Default implementation uses a ThreadPool
+           to parallelize query processing."""
+        pool = ThreadPool()
+        if self.metric == 'angular':
+            X = sklearn.preprocessing.normalize(X, axis=1, norm='l2')
+        X = X.tolist()
+        num_candidates = n + self.fanout
+        self.res = pool.map(lambda q: self.run_knn_query(num_candidates=num_candidates, n=n, q=q), X)
